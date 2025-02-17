dbb1a59012d27621323441e962c48432cd709850
tycowong <33732733+tycowong@users.noreply.github.com>
2025-02-16 18:01:50 -0800

    Added chadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button.tsx"
 
 export default function Home() {
   return (
@@ -56,6 +57,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
+
           <Image
             aria-hidden
             src="/file.svg"
@@ -65,6 +67,7 @@ export default function Home() {
           />
           Learn
         </a>
+        <Button> Test </Button>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
